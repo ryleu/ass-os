cecb183bdb0ae3833d9e5e5c639bb2e8d950ccf9
ryleu <69326171+ryleu@users.noreply.github.com>
2025-03-04 16:56:45 -0600

    install vscode properly

--- build.sh
+++ build.sh
@@ -2,13 +2,23 @@
 
 set -ouex pipefail
 
+# install dnf plugins and rpmfusion
 dnf5 -y install dnf5-plugins \
         https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
         https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# import microsoft signing key
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# add vscode repo
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+# install docker repo and docker
 dnf5 -y config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
 dnf5 -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable docker
+
+# install vscode
+dnf5 -y install code
 
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package

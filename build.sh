38f22e7af4c7a0b16d813e82463aa636d6526213
ryleu <69326171+ryleu@users.noreply.github.com>
2025-03-04 16:04:01 -0600

    stop using --now

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ dnf5 -y install dnf5-plugins \
 
 dnf5 -y config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
 dnf5 -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl enable --now docker
+systemctl enable docker
 
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package

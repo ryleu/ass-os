e41764fb49fb0f642197ce662a57faf324ce1e3e
ryleu <69326171+ryleu@users.noreply.github.com>
2025-03-04 15:59:29 -0600

    install docker

--- Containerfile
+++ Containerfile
@@ -15,10 +15,6 @@ FROM ghcr.io/ublue-os/silverblue-main:latest
 
 COPY build.sh /tmp/build.sh
     
-RUN dnf5 -y install dnf5-plugins \
-        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
--- build.sh
+++ build.sh
@@ -2,23 +2,16 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf5 -y install dnf5-plugins \
+        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 -y config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable --now docker
 
-# this installs a package from fedora repos
-dnf install -y tmux 
-
-# Use a COPR Example:
-#
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
